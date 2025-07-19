b143cde87495355f06b4a4e5ecd023d1affaf5bc
GK <gosha.korobitskiy@gmail.com>
2025-07-19 14:38:24 +0700

    __second Version__19.07.2025__ _add login to interface and video player but interface looks bad now_

--- Client_GUI/Uni_Tests_Pytest/main.py
+++
@@ -1,9 +0,0 @@
-prime_nums = []
-for num in range(1, 50):
-    for div in range(2, num):
-        print(num%div)
-        if num % div == 0:
-            break
-    else:
-        prime_nums.append(num)
-print(prime_nums)
